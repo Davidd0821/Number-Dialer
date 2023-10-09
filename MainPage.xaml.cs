31286052ef51daa72ce1417a1ef8f13e6328073f
Davidd0821 <126388661+Davidd0821@users.noreply.github.com>
2023-10-09 19:16:25 +0700

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ACT2Mid;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    async void onDial(object sender, EventArgs e)
+    {
+
+        try
+        {
+            string phonenum = PhoneNum.Text.Trim();
+            string phonepattern = @"\d{10}$";
+
+            if (Regex.IsMatch(phonenum, phonepattern))
+            {
+                PhoneDialer.Open(PhoneNum.Text);
+
+            }
+
+            else
+            {
+                await DisplayAlert("Invalid input", "Please enter a valid phone number", "OK");
+            }
+        }
+
+        catch (Exception ex)
+        {
+            await DisplayAlert(ex.Message, "Error");
+        }
+    }
+
+    private Task DisplayAlert(string message, string v)
+    {
+        throw new NotImplementedException();
+    }
+}
+
